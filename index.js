a2716064946452e55919f7abbc669603ccb08007
Stephen Gray <hello@stephengray.io>
2022-10-21 11:57:34 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import ElCrudForm from './src/Form';
-import ElCrudList from './src/List';
+import ElCrudForm from './src/Form.vue';
+import ElCrudList from './src/List.vue';
 
 export {
     ElCrudForm,
